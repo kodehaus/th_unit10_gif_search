324b4835be40cb232ef77d6e971ec88c4c9f1952
Ken Robinson <krobinson@kodehaus.com>
2021-03-31 02:56:31 -0400

    Final gif search updates

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,35 @@
-import React from 'react'
 import '../App.css';
 import axios from 'axios';
-
+import React, { useState, useEffect } from 'react';
 import SearchForm from './SearchForm';
 import GifList from './GifList';
+import Gif from './Gif';
 
-function App() {
-
+function App () {
+  const [data, setData] = useState([]); // [0, f]
+  const [query, setQuery] = useState('cats'); // [0, f]
+  const [isLoading, setIsLoading] = useState(true); // [0, f]
+  const performSearch = (value) => setQuery(value);
+  useEffect(() => {
+    axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=7RKHACAQ0qHlL0ClPfKqhmoiBVBWnJPH`)
+      .then(response => setData(response.data.data))
+      .catch(error => console.log('Error fetching and parsing data', error))
+      .finally(() => setIsLoading(false));
+  }, [query]);
   return (
     <>
-      <div className="main-header">
-        <div className="inner">
-          <h1 className="main-title">GifSearch</h1>
-          <SearchForm />
+      <div className='main-header'>
+        <div className='inner'>
+          <h1 className='main-title'>GifSearch</h1>
+          <SearchForm onSearch={performSearch} />
         </div>
       </div>
-      <div className="main-content">
+      <div className='main-content'>
+        {
+          isLoading
+            ? <p> Loading.......</p>
+            : <GifList data={data} />
+        }
 
       </div>
     </>
